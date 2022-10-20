2636fa8f4e382ba3f708131c24c728fcf44e8012
Slava K <slava.kostromin.2000@mail.ru>
2022-10-20 21:30:07 +0700

    41

---
+++ Z_41/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте,
+сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+
+Console.Clear();
+Console.Write("Сколько чисел ты хочешь ввести: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"чисел больше нуля: {sum}");
